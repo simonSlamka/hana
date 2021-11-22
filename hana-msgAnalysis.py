5c20aa671fb6338d37fcf4ca3f8e422467ac6f8d
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2021-11-22 08:49:01 +0100

    added flair, going to test and see results

--- hana-msgAnalysis.py
+++ hana-msgAnalysis.py
@@ -17,7 +17,10 @@ sqlite>
 
 from nltk.sentiment import SentimentIntensityAnalyzer
 import demoji
-import flair
+from flair.models import TextClassifier
+from flair.data import Sentence
+
+classifier = TextClassifier.load("en-sentiment")
 
 sia = SentimentIntensityAnalyzer()
 
@@ -27,6 +30,7 @@ count = 0
 msgSentimentTotal = 0
 msgSentimentPosTotal = 0
 msgSentimentNegTotal = 0
+msgFlairSentimentTotal = 0
 
 for line in msg:
     count += 1
@@ -34,11 +38,14 @@ for line in msg:
     msgSentiment = sia.polarity_scores(msg)["compound"]
     msgSentimentPos = sia.polarity_scores(msg)["pos"]
     msgSentimentNeg = sia.polarity_scores(msg)["neg"]
+    sentence = Sentence(msg)
     msgSentiment = (msgSentiment+1)/2*100
     msgSentimentTotal += msgSentiment
     msgSentimentAvg = msgSentimentTotal/count
     msgSentimentPosTotal += msgSentimentPos
     msgSentimentNegTotal += msgSentimentNeg
+    classifier.predict(sentence)
+    msgFlairSentimentTotal += sentence.labels[0].to_dict()["confidence"]
 
 print("Total message sentiment: ", msgSentimentTotal)
 print("Message count:", count)
@@ -46,3 +53,5 @@ print("Average message sentiment: ", msgSentimentAvg)
 
 print("Avg positive sentiment:", msgSentimentPosTotal/count)
 print("Avg neg sentiment:", msgSentimentNegTotal/count)
+
+print("Total sentiment as predicted by flair: ", msgFlairSentimentTotal)
